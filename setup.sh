171d0126e0d6424b2b8fc9e28099e8f9211ed229
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-04-01 09:00:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 #******************************************** #startup-sequence  :-) ************* ********************************************
 
 clear
-echo "Welcome to KickPi-OS"
+echo "Welcome to KickOS"
 echo " "
 echo " "
 
-      sudo cp -R /home/$USER/KickPi-OS/scripts/* /usr/local/bin
+      sudo cp -R /home/$USER/KickOS/scripts/* /usr/local/bin
       sudo apt -y update
       whiptail --msgbox "Optional: \n  \n  Now insert your * AMIGA * USB stick with the \n  * Shared * folder into the Raspberry" 15 50 1
 
@@ -34,19 +34,10 @@ if    [ ! -d "/media/pi/AMIGA/Shared/" ]; then
 	  
           fi
 
-if    [ ! -d "/media/usb0/Shared/" ]; then
-          clear
-	else
-	  clear
-          echo "***  Amiga Forever files found ***"
-	  echo " "
-	  echo "... copy files to Amibian will take 2-5 min "
-	  cp -rf /media/usb0/Shared/* ~/Amiga  
-	  
-          fi
+
 	
-sudo cp -R /home/$USER/KickPi-OS/scripts/* /usr/local/bin
-cp  /home/$USER/KickPi-OS/scripts/bashrc /home/$USER/.bashrc
+sudo cp -R /home/$USER/KickOS/scripts/* /usr/local/bin
+cp  /home/$USER/KickOS/scripts/bashrc /home/$USER/.bashrc
 sudo raspi-config nonint get_config_var gpu_mem_256 /boot/config.txt
 sudo raspi-config nonint do_boot_behaviour B2
 sudo raspi-config nonint get_ssh
