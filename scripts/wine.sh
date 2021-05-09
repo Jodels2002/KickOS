b7c8d3080e1e6b9d021f0ea34cffb732aa0ca3eb
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-05-09 09:17:02 +0200

    Update wine.sh

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,5 +1,8 @@
 
 sudo dpkg --add-architecture i386 && sudo apt update
+sudo apt -y install linux-image-686 linux-headers-686
+
+
 
 sudo apt -y install gnupg2 software-properties-common
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
