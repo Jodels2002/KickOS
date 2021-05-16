b422aed430562a9a1c0a4ea046a2eae2e76c1109
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-05-16 05:30:17 +0100

    Update amitools.sh

--- scripts/amitools.sh
+++ scripts/amitools.sh
@@ -1,7 +1,7 @@
 
 sudo python3 -m pip install -U pip
 sudo python3 -m pip install -U setuptools
-#sudo apt-get install python3-pip
+#https://github.com/henrikstengaard/amiga-workbench-setup.git
 sudo pip install amitools
 #sudo pip3 install -U  git+https://github.com/cnvogelg/amitools.git
 #xdftool mydisk.hdf unpack
