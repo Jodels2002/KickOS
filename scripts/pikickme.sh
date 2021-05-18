57a45329ecd579fa875db013811619c5e8a6e194
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-05-18 08:44:45 +0200

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -286,17 +286,12 @@ fi
       
       
        
-sudo dpkg --add-architecture i386 && sudo apt update
-sudo apt -y install linux-image-686 linux-headers-686
-
-
-
 sudo apt -y install gnupg2 software-properties-common
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo apt-add-repository https://dl.winehq.org/wine-builds/debian/
 sudo apt update
-sudo apt install --install-recommends winehq-stable
-#sudo apt -y install wine 
+sudo apt install --install-recommends wine
+
 wine msiexec /i ~/Desktop/A      wine32 \
       wine64 \
       libwine \
@@ -307,8 +302,7 @@ wine msiexec /i ~/Desktop/A      wine32 \
       cd
       wget -nc https://download.abime.net/winuae/releases/InstallWinUAE4400_x64.msi
       #wine msiexec /i ~/Desktop/AmigaForever.msi
-      wine msiexec /i InstallWinUAE4400_x64.msi
-      
+      wine msiexec /i wine msiexec /i InstallWinUAE4400_x64.msi
       fi
       
 
