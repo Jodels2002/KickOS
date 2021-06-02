3026feb7431c3b05762ce37f23b2813947e6d3fb
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-06-02 05:25:50 +0100

    Update makeiso.sh

--- scripts/makeiso.sh
+++ scripts/makeiso.sh
@@ -1,3 +1,11 @@
+sudo chmod -R 777  /home/pi/Documents/
+sudo unsquashfs -f -d /home/pi/Documents/KickOS/ /home/pi/Documents/temp/live/filesystem.squashfs
+sudo chmod -R 777  /home/pi/Documents/
+cd /home/pi/Documents/
+
+sudo mksquashfs /home/pi/Documents/KickOS/  filesystem.squashfs
+
+
 
 sudo apt install -y genisoimage
 sudo rm -rf /tmp
