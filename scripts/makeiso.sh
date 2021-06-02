d8e02da23ed3140b485fc6cc81b5119c10d3b1a9
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-06-02 05:53:34 +0100

    Update makeiso.sh

--- scripts/makeiso.sh
+++ scripts/makeiso.sh
@@ -1,3 +1,4 @@
+sudo apt install -y genisoimage
 sudo chmod -R 777  /home/pi/Documents/
 sudo unsquashfs -f -d /home/pi/Documents/KickOS/ /home/pi/Documents/temp/live/filesystem.squashfs
 sudo chmod -R 777  /home/pi/Documents/
@@ -7,11 +8,6 @@ sudo mksquashfs /home/pi/Documents/KickOS/  filesystem.squashfs
 
 
 
-sudo apt install -y genisoimage
-sudo rm -rf /tmp
-sudo mkdir /tmp
 
-git clone --depth=1 https://github.com/Tomas-M/linux-live.git
-chmod -R 777 linux-live
-cd linux-live
-sudo ./build 
+cd /home/pi/Documents/
+sudo genisoimage -J -l -R -V "KickOS" -iso-level 4 -o KickOS.iso /home/pi/Documents/temp/
